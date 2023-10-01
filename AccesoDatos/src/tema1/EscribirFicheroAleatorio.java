695b69a7b5cfbe9bab51679b6b5487e04da9b459
elyassdam <95480897+elyassdam@users.noreply.github.com>
2023-10-01 15:37:58 +0200

    Actualizar EscribirFicheroAleatorio.java

--- AccesoDatos/src/tema1/EscribirFicheroAleatorio.java
+++ AccesoDatos/src/tema1/EscribirFicheroAleatorio.java
@@ -12,17 +12,17 @@ public class EscribirFicheroAleatorio {
 		String apellido[]= {"Fernandez","Ramos,Alvarez","Gonzalez","Lopez"};
 		int dep []= {10,20,10,10,30};//departamentos
 		Double salario[]= {1000.45,2400.60,3000.0,1500.56,2200.0};//salarios
-		StringBuffer buffer=null;//buffer para almacenar apellido
-		int n=apellido.length;
+		StringBuffer buffer=null;//buffer para almacenar apellido seria como si fuese un array y lo inicializamos a null  
+		int n=apellido.length;//guardamos el tamaño del String de apellido 
 		for (int i = 0; i < n; i++) {
-			file.writeInt(i+1);
+			file.writeInt(i+1);//Escribe el id partiendo de 1 
 			buffer=new StringBuffer(apellido[i]);
-			buffer.setLength(10);
-			file.writeChars(buffer.toString());
+			buffer.setLength(10);//Le indicamos el espacio que tendran todos los apellidos siendo de 10
+			file.writeChars(buffer.toString());//Pasamos el buffer a String
 			file.writeInt(dep[i]);//insertar departamento
 			file.writeDouble(salario[i]);//inserta salario
 		}
-		file.close();
+		file.close();//Cerramos el fichero
 		}
 		
 	}
